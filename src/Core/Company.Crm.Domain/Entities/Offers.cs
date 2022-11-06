1e0b53375e88780c0d110473182665e06e588fd2
alikemaluysal <alikemaluysal4235@gmail.com>
2022-11-06 13:22:24 +0300

    Offers entity eklendi

---
+++ src/Core/Company.Crm.Domain/Entities/Offers.cs
@@ -0,0 +1,12 @@
+﻿using Company.Framework.Entity;
+
+namespace Company.Crm.Domain.Entities
+{
+    public class Offers : BaseEntity
+    {
+        public int RequestId { get; set; }
+        public int EmployeeUserId { get; set; }
+        public DateTime? OfferDate { get; set; }
+        public int? BidAmount { get; set; }
+    }
+}
