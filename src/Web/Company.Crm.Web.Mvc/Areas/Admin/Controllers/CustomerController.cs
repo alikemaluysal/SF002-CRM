289d19bef880cbdc79d349115a253ed5d9962731
alikemaluysal <alikemaluysal4235@gmail.com>
2022-11-13 13:55:07 +0300

    Web tarafında service CRUD örnekleri eklendi

---
+++ src/Web/Company.Crm.Web.Mvc/Areas/Admin/Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿using Company.Crm.Application.Dtos;
+using Company.Crm.Application.Services.Abstracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Company.Crm.Web.Mvc.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CustomerController : Controller
+    {
+        private readonly ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        public IActionResult Index(int page = 1)
+        {
+            var customers = _customerService.GetPaged(page);
+            return View(customers);
+        }
+
+        public IActionResult Save(CreateOrUpdateCustomerDto dto)
+        {
+            var result = _customerService.Insert(dto);
+            return RedirectToAction("Index");
+        }
+    }
+}
