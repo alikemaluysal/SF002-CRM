854ecd5cba0d434ad4390f7f271cfe2e4363b984
alikemaluysal <alikemaluysal4235@gmail.com>
2023-01-08 12:39:34 +0300

    GetPaged ve axios düzenlemeleri

--- src/Web/company-crm-web-vue/src/pages/customer/CustomerList.vue
+++ src/Web/company-crm-web-vue/src/pages/customer/CustomerList.vue
@@ -1,25 +1,44 @@
 <script setup>
 import { ref, onMounted } from 'vue';
-import axios from 'axios';
-const customerList = ref([]);
+const dataList = ref([]);
+
 onMounted(() => {
-	axios.get('https://localhost:7177/api/customer')
-		.then(function (response) {
-			customerList.value = response.data;
-		})
+	fetchItems();
 })
+
+function fetchItems(page = 1) {
+	axios.get('customer/getpaged', { params: { page, perPage: 10 } }).then(response => {
+		dataList.value = response.data.data;
+	})
+}
 </script>
 <template>
+	<div class="btn-group float-end">
+		<button class="btn btn-primary" @click="createItem">Yeni</button>
+		<button class="btn btn-primary" @click.prevent="fetchItems(1)">Yenile</button>
+	</div>
+
 	<h1>Customer List</h1>
 	<table class="table table-bordered">
 		<thead>
 			<tr>
-				<th>companyName</th>
+				<th>#</th>
+				<th>Company</th>
+				<th>Gender</th>
+				<th></th>
 			</tr>
 		</thead>
 		<tbody>
-			<tr v-for="customer of customerList">
-				<td>{{ customer.companyName }}</td>
+			<tr v-for="{ id, companyName, genderName } in dataList" :key="id">
+				<td>{{ id }}</td>
+				<td>{{ companyName }}</td>
+				<td>{{ genderName }}</td>
+				<td>
+					<button class="btn btn-success btn-sm me-2" @click="editItem(id)">
+						Düzenle
+					</button>
+					<button class="btn btn-danger btn-sm" @click="deleteItem(id)">Sil</button>
+				</td>
 			</tr>
 		</tbody>
 	</table>
