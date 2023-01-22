e7493574844c58d2961dcba963c744814d244cba
alikemaluysal <alikemaluysal4235@gmail.com>
2023-01-22 14:18:27 +0300

    Bazı kodlar ServiceRegistration içerisine alındı

--- src/Web/Company.Crm.Web.Api/Program.cs
+++ src/Web/Company.Crm.Web.Api/Program.cs
@@ -1,13 +1,8 @@
 using Company.Crm.Application;
 using Company.Crm.Entityframework;
-using Company.Framework.Dtos;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Diagnostics;
-using Microsoft.IdentityModel.Tokens;
+using Company.Crm.Web.Api;
+using Company.Crm.Web.Api.Middlewares;
 using Microsoft.OpenApi.Models;
-using System.ComponentModel.DataAnnotations;
-using System.Text;
-using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -43,45 +38,9 @@ builder.Services.AddSwaggerGen(option =>
         }
     });
 });
-
 builder.Services.AddEntityFrameworkRegistration(builder.Configuration);
 builder.Services.AddApplicationRegistration(builder.Configuration);
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: "CorsAyari", policy =>
-    {
-        policy
-            .WithOrigins(builder.Configuration["App:ClientUrls"].Split(','))
-            .AllowAnyHeader().AllowAnyMethod().AllowCredentials();
-
-        // Tüm adreslere izin verme
-        //policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod().AllowCredentials();
-    });
-});
-
-builder.Services
-    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            // Yetkilendirmeyi yapan
-            ValidateIssuer = true,
-            ValidIssuer = builder.Configuration["Auth:Jwt:Issuer"],
-
-            // Yetkilendirmeyi kullanan client
-            ValidateAudience = true,
-            ValidAudience = builder.Configuration["Auth:Jwt:Audience"],
-
-            // Token zaman doðrulamasý
-            ValidateLifetime = true,
-
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Auth:Jwt:SecurityKey"]))
-        };
-    });
-
+builder.Services.AddApiRegistration(builder.Configuration);
 
 var app = builder.Build();
 
@@ -92,58 +51,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-var isProduction = app.Environment.IsProduction();
-
-app.UseExceptionHandler(errorApp =>
-{
-    errorApp.Run(async context =>
-    {
-        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-
-        var errorMessage = "Error!";
-
-        var error = context.Features.Get<IExceptionHandlerPathFeature>();
-
-        var exception = error?.Error;
-
-        if (exception is FileNotFoundException)
-        {
-            errorMessage = "File Not Found!";
-        }
-        else if (exception is UnauthorizedAccessException)
-        {
-            //context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
-            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-            errorMessage = "Unauthorized Access to resource!";
-        }
-        else if (exception is ValidationException)
-        {
-            context.Response.StatusCode = StatusCodes.Status422UnprocessableEntity;
-            errorMessage = "Validation error!";
-        }
-        else
-        {
-            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-            errorMessage = "Internal error!";
-        }
-
-        if (!isProduction) errorMessage = exception.Message;
-
-        var errorResponse = new ServiceResponse<string>(errorMessage);
-        var errorBody = JsonSerializer.Serialize(errorResponse);
-
-        context.Response.ContentType = "application/json";
-        await context.Response.WriteAsync(errorBody);
-    });
-});
-
+app.UseExceptionHandler(errorApp => errorApp.UseGlobalExceptionHandling(app.Environment.IsProduction()));
 app.UseHttpsRedirection();
-
-app.UseCors("CorsAyari");
-
+app.UseCors("CrmCors");
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
---
+++ src/Web/Company.Crm.Web.Api/ServiceRegistrations.cs
@@ -0,0 +1,54 @@
+﻿using Company.Crm.Web.Api.Jobs;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Company.Crm.Web.Api;
+
+public static class ServiceRegistrations
+{
+    public static void AddApiRegistration(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy(name: "CrmCors", policy =>
+            {
+                policy
+                    .WithOrigins(configuration["App:ClientUrls"].Split(','))
+                    .AllowAnyHeader().AllowAnyMethod().AllowCredentials();
+
+                // Tüm adreslere izin verme
+                //policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod().AllowCredentials();
+            });
+        });
+
+        services
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    // Yetkilendirmeyi yapan
+                    ValidateIssuer = true,
+                    ValidIssuer = configuration["Auth:Jwt:Issuer"],
+
+                    // Yetkilendirmeyi kullanan client
+                    ValidateAudience = true,
+                    ValidAudience = configuration["Auth:Jwt:Audience"],
+
+                    // Token zaman doğrulaması
+                    ValidateLifetime = true,
+                    //ClockSkew = TimeSpan.FromMinutes(1),
+                    ClockSkew = TimeSpan.Zero,
+
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Auth:Jwt:SecurityKey"]))
+                };
+            });
+
+
+        // Background Task
+        services.AddHostedService<TimedHostedService>();
+        services.AddHostedService<WeeklyEmailHostedService>();
+    }
+}
