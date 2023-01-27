96c14474d4c47ae2161dfa420ce96043e723b873
alikemaluysal <alikemaluysal4235@gmail.com>
2023-01-27 22:05:58 +0300

    layout düzenleme

--- src/Web/company-crm-web-vue/src/views/_layouts/MasterLayout.vue
+++ src/Web/company-crm-web-vue/src/views/_layouts/MasterLayout.vue
@@ -1,25 +1,12 @@
 <template>
-	<div class="page">
-		<Navbar />
-		<div class="page-wrapper">
-			<router-view></router-view>
-			<Footer />
-		</div>
+	<Navbar />
+	<div class="page-wrapper">
+		<router-view></router-view>
+		<Footer />
 	</div>
 </template>
 
 <script setup>
-import { onBeforeMount } from 'vue'
-import { useRouter } from 'vue-router'
-import session from '@/plugins/session'
 import Navbar from '@/components/Navbar.vue'
 import Footer from '@/components/Footer.vue'
-const router = useRouter()
-
-onBeforeMount(() => {
-	// if (session.needsAuthentication()) {
-	// 	window.localStorage.clear();
-	// 	router.push("/auth/login");
-	// }
-})
 </script>
